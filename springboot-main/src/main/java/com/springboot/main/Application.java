10f2c9c481875f17574a63d843706ddd969daf9d
yilei111 <2714512416@qq.com>
2019-05-02 19:03:48 +0800

    提交

--- springboot-main/src/main/java/com/springboot/main/Application.java
+++ springboot-main/src/main/java/com/springboot/main/Application.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 主启动入口
+ * 主启动入口 
  * @author Mr yi
  */
 @SpringBootApplication
